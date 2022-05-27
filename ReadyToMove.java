7e331af6b03e890ea7bb0c0cbfe5285a08e02cd5
Tony Liu <tony.liu@mail-lggram.com>
2022-05-27 21:47:29 +0800

    Adding new Java File,and It will be moved into another Folder

---
+++ ReadyToMove.java
@@ -0,0 +1,13 @@
+/*
+ * ReadyToMove.java
+ * Author: Tony Liu
+ *
+ *
+ *
+ *
+ * */
+public class ReadyToMove {
+	public static void main(String[] args){
+		System.out.println("ReadyToMove Java File ! ");
+	}
+}
