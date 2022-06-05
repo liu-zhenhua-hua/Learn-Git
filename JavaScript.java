9291507fb677bb4c3a84aa2d67b80f67d2458ac5
Tony Liu <tony.liu@mail-lggram.com>
2022-06-05 11:32:47 +0800

    Adding JavaScript Java File

---
+++ JavaScript.java
@@ -0,0 +1,13 @@
+/*
+ * File Name: JavaScript.java
+ * Author : Tony Liu
+ * Description: JavaScript Java File
+ *
+ *
+ *
+ * */
+public class JavaScript {
+	public static void main(String[] args){
+		System.out.println("JavaScript ");
+	}
+}
